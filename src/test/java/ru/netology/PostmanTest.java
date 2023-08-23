c7c7240993ad39eb81c444ebf1b80531771fca0e
Steinrabe <morgenstern-07@mail.ru>
2023-08-23 22:07:37 +0300

    Update 3

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
 // Проверки
                 .then().log().all()
                 .statusCode(200)
-                .body("data", equalTo("same data"))
+                .body("data", equalTo("some data"))
         ;
 
     }
